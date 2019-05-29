1dcddfd11f8731aa947f806a21a65a0c371457cf
Paramvir Singh <paramvir109@gmail.com>
2019-05-29 18:46:32 +0530

    Update deploy and readme

--- deploy.js
+++ deploy.js
@@ -2,6 +2,8 @@ const ghp = require('gh-pages');
 
 ghp.publish('public', {
   branch: 'master',
+  repo: 'https://github.com/Paramvir109/Paramvir109.github.io.git',
+
 }, err => {
   if (err) {
     console.log(err);
